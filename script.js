b1a5bbf984277997ccff24df40e467f3610a89bb
tnfh23-web <tnfh23@gmail.com>
2026-01-04 23:11:01 +0900

    sec-3 scrolltrigger

---
+++ script.js
@@ -0,0 +1,41 @@
+// 스크롤 트리거 플러그인 활성화
+gsap.registerPlugin(ScrollTrigger);
+// GSAP scrollTrigger ------------------------------ //
+function scrollTrigger__init() {
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".sec-3 .content-wrap",
+      start: "top top",
+      end: "+=800%",
+      pin: true,
+      scrub: 1,
+      anticipatePin: 1,
+      invalidateOnRefresh: true,
+    },
+  });
+
+  tl.set(".sec-3-text", { opacity: 0 });
+
+  tl.to(".sec-3 .bg-container img", {
+    scale: 1,
+    ease: "none",
+    duration: 3,
+  });
+
+  // 2) 텍스트 시퀀스 (bg 커진 다음 이어서)
+  tl.fromTo(".t1", { opacity: 0, y: 20 }, { opacity: 1, y: 0, duration: 1.2 })
+    .to(".t1", { opacity: 0, duration: 0.4 })
+
+    .fromTo(".t2", { opacity: 0, y: 20 }, { opacity: 1, y: 0, duration: 1.2 })
+    .to(".t2", { opacity: 0, duration: 0.4 })
+
+    .fromTo(".t3", { opacity: 0, y: 20 }, { opacity: 1, y: 0, duration: 1.2 })
+    .to(".t3", { opacity: 0, duration: 0.4 })
+
+    .fromTo(".t4", { opacity: 0, y: 20 }, { opacity: 1, y: 0, duration: 1.2 })
+    .to(".t4", { opacity: 0, duration: 0.4 })
+
+    .fromTo(".t5", { opacity: 0, y: 20 }, { opacity: 1, y: 0, duration: 1.2 });
+}
+
+window.addEventListener("load", scrollTrigger__init);
